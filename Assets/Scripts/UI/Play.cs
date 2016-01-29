5c8c0eee656f963cc85071a423559eb5d6596910
BobSmun <somemadeupname@gmail.com>
2016-01-30 01:49:11 +0800

    Add a menu

---
+++ Assets/Scripts/UI/Play.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
