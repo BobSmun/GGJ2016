de022f2f920d518a01ee194a4a34fb3b34afe68b
BobSmun <somemadeupname@gmail.com>
2016-01-30 21:32:17 +0800

    Add a way to detect the end of the 'race'

---
+++ Assets/Scripts/EndRace.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndRace : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
