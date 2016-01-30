ca99cefa9e4c6d5fc45955d74f35e72d46e8304d
BobSmun <somemadeupname@gmail.com>
2016-01-30 16:54:05 +0800

    Add HUD with timer

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeCounter = 0f;
+    public bool running = true;
+    
+    void FixedUpdate()
+    {
+        if(running)
+        {
+            TimeCounter += Time.fixedDeltaTime;
+            gameObject.GetComponent<Text>().text = TimeCounter.ToString("F2");
+        }
+    }
+}
