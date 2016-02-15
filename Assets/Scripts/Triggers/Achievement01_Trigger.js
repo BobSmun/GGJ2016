f9e7215059bfab8ac205005d4cb8e0e08955bfb9
gibias <gabrielibias@gmail.com>
2016-02-15 19:25:36 +0800

    Added a few more sound effects

--- Assets/Scripts/Triggers/Achievement01_Trigger.js
+++ Assets/Scripts/Triggers/Achievement01_Trigger.js
@@ -3,14 +3,12 @@
 import UnityEngine.UI;
 
 var achievementScript: AchievementTracker;
-var mainDoorTrigger: GameObject;
  
 function OnTriggerEnter(c:Collider){
 
     if(c.gameObject.tag =="Player"){
 
-        achievementScript.firstRun = true;
-        mainDoorTrigger.SetActive(true);        
+        achievementScript.firstRun = true;           
         Destroy(this);
     }
 }
